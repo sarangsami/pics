02a8e99d8bffa975fde749c377a70907ca13358c
Sarang <sarangsami75@gmail.com>
2020-03-24 13:15:02 +0430

    Updates

---
+++ precache-manifest.3a0b35048daee1235921e528a64b26f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633ecd5eb4372d525c0304d0ab4fca8a",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "0f264ec78e0e76683f8d",
+    "url": "/pics/static/css/main.df47ecdf.chunk.css"
+  },
+  {
+    "revision": "a2f96f1b45bab8aa1c5e",
+    "url": "/pics/static/js/2.bf9a6dbf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pics/static/js/2.bf9a6dbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f264ec78e0e76683f8d",
+    "url": "/pics/static/js/main.d2adcdd2.chunk.js"
+  },
+  {
+    "revision": "2251b8dd8072a085af31",
+    "url": "/pics/static/js/runtime-main.302674cc.js"
+  }
+]);
